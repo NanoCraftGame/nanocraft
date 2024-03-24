f51a648349708184f0470f8fcf650a5d99eefa17
Roman Bekkiev <rb@rblab.net>
2024-03-24 21:11:45 +0100

    use secret in hook

--- hook.js
+++ hook.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import fs from 'fs'
 
 const server = http.createServer((req, res) => {
 	if (req.method === 'POST') {
@@ -9,8 +10,22 @@ const server = http.createServer((req, res) => {
 		})
 
 		req.on('end', () => {
-			// Process the received data here
-			console.log('Received data:', body)
+			// read .env file using fs
+			const env = fs.readFileSync('.env', 'utf-8')
+			// parse the content of the .env file
+			const envLines = env.split('\n')
+			const envVars = {}
+			envLines.forEach((line) => {
+				const [key, value] = line.split('=')
+				envVars[key] = value
+			})
+			// compare body with the secret key
+			if (body === envVars.PULL_TOKEN) {
+				// print user under which the hook is running
+				console.log('Current user: ', process.env.USER)
+			} else {
+				console.log('Invalid secret key')
+			}
 
 			// Send a response
 			res.statusCode = 200
