9920e2b571bedea5f1b829993c5b423f6d41e528
Erball-M <119080597+Erball-M@users.noreply.github.com>
2024-04-09 13:15:11 +0600

    added popup for tasks description

---
+++ src/routes/project/TaskDescription.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Task } from '$lib/model/tasks'
+	import Backdrop from '$lib/components/Backdrop.svelte'
+	import Panel from '$lib/components/Panel.svelte'
+	import { escape } from '$lib/directives/escape'
+	export let isOpen: boolean = false
+	export let task: Task | null
+	export let close: VoidFunction
+</script>
+
+<Backdrop {isOpen}>
+	<Panel>
+		<div class="container" use:escape on:escape={close}>
+			<h3 class="title">{task?.name}</h3>
+			<div class="description">{task?.description}</div>
+			<div class="additional-data">
+				<p class="status">STATUS: {task?.status}</p>
+				<p class="estimate">ESTIMATE TIME: {task?.estimate}</p>
+			</div>
+		</div>
+	</Panel>
+</Backdrop>
