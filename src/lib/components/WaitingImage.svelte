167d4c577a6e245e4365c8a01d061b2cf29a8f46
Erball-M <119080597+Erball-M@users.noreply.github.com>
2024-04-16 14:21:31 +0600

    try to add srcset. Have to be reviewed

--- src/lib/components/WaitingImage.svelte
+++ src/lib/components/WaitingImage.svelte
@@ -7,10 +7,11 @@
 	export let height: number
 	export let style = ''
 	style = `width: ${width}px; height: ${height}px; ${style}`
+	const sizes = `(min-width: 1200px) ${width}px, (min-width: 992px) ${width * 0.9}px,(min-width: 768px) ${width * 0.9 ** 2}px, ${width * 0.9 ** 3}px`
 </script>
 
 {#if $src}
-	<enhanced:img {style} src={$src} {alt} {...$$restProps} />
+	<enhanced:img {style} src={$src} {alt} {sizes} {...$$restProps} />
 {:else}
 	<div {style} {...$$restProps} />
 {/if}
